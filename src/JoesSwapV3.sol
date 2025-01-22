c6042cbb47992039c3d0d9bd9da5147845cc31e3
joesepherus <joesepherus@gmail.com>
2025-01-22 23:36:28 +0800

    Init with the JoesSwapV2 base implemnetation.

---
+++ src/JoesSwapV3.sol
@@ -0,0 +1,400 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+ * @title JoesSwapV3
+ * @author Joesepherus
+ * @dev Decentralized token swapping contract with liquidity provision and fee management.
+ */
+contract JoesSwapV3 is ReentrancyGuard, Ownable {
+    using SafeERC20 for IERC20;
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+    uint256 public reserve0;
+    uint256 public reserve1;
+    uint256 public liquidity;
+    uint256 accumulatedFeePerLiquidityUnitToken0;
+    uint256 accumulatedFeePerLiquidityUnitToken1;
+    bool public poolInitialized = false;
+
+    mapping(address => uint256) public lpBalances;
+    mapping(address => uint256) public userEntryFeePerLiquidityUnitToken0;
+    mapping(address => uint256) public userEntryFeePerLiquidityUnitToken1;
+
+    /*//////////////////////////////////////////////////////////////
+                                CONSTANTS 
+    //////////////////////////////////////////////////////////////*/
+    uint256 immutable PRECISION = 1e18;
+    uint256 immutable FEE = 3;
+    uint256 immutable ONE_HUNDRED = 100;
+
+    /*//////////////////////////////////////////////////////////////
+                                EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event PoolInitialized(
+        address indexed sender,
+        uint256 indexed amount0,
+        uint256 indexed amount1
+    );
+    event AddLiquidity(
+        address indexed sender,
+        uint256 indexed amount0,
+        uint256 indexed amount1
+    );
+    event RemoveLiquidity(
+        address indexed sender,
+        uint256 liquidityToRemove,
+        uint256 indexed amount0,
+        uint256 indexed amount1
+    );
+    event Swap(
+        address indexed sender,
+        uint256 indexed amount0,
+        uint256 indexed amount1
+    );
+    event WithdrawFees(address indexed sender, uint256 indexed feeAmount);
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error InsufficentFeesBalance();
+    error InsufficentLiquidity();
+    error PoolAlreadyInitialized();
+
+    constructor(address _token0, address _token1) Ownable(msg.sender) {
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Initializes the pool with the provided liquidity amounts of token0 and token1.
+     * @dev This function can only be called once and by the owner.
+     *      It transfers the specified amounts of token0 and token1 from the caller to the contract,
+     *      calculates the new liquidity, updates reserves, and records the user's entry per liquidity unit.
+     *      Emits a `PoolInitialized` event upon successful execution.
+     * @param amount0 The amount of token0 to add to the pool.
+     * @param amount1 The amount of token1 to add to the pool.
+     * @custom:modifier onlyOwner Can only be called by the contract owner.
+     * @custom:revert PoolAlreadyInitialized if the pool has already been initialized.
+     */
+    function initializePoolLiquidity(
+        uint256 amount0,
+        uint256 amount1
+    ) external onlyOwner {
+        if (poolInitialized) revert PoolAlreadyInitialized();
+        uint256 amount0Scaled = amount0 * PRECISION;
+        uint256 amount1Scaled = amount1 * PRECISION;
+        uint256 newLiquidity = sqrt(amount0Scaled * amount1Scaled);
+
+        reserve0 += amount0;
+        reserve1 += amount1;
+
+        userEntryFeePerLiquidityUnitToken0[
+            msg.sender
+        ] = accumulatedFeePerLiquidityUnitToken0;
+        userEntryFeePerLiquidityUnitToken1[
+            msg.sender
+        ] = accumulatedFeePerLiquidityUnitToken1;
+        liquidity += newLiquidity;
+        lpBalances[msg.sender] += newLiquidity;
+        poolInitialized = true;
+
+        emit PoolInitialized(msg.sender, amount0, amount1);
+
+        token0.safeTransferFrom(msg.sender, address(this), amount0);
+        token1.safeTransferFrom(msg.sender, address(this), amount1);
+    }
+
+    /**
+     * @notice Adds liquidity to the pool with the provided amount of token0 and token1 is then calculated.
+     * @dev The function scales up the amount of token0 by PRECISION.
+     *      It calls getAmountOut to get the correct amount of token1 in proportion to token0.
+     *      Calculates the liquidity, updates reserves, handles transfers from user to the pool.
+     *      Sets up liquidity balance and entry point for the caller.
+     *      Emits a `AddLiquidity` event upon successful execution.
+     * @param amount0 The amount of token0 to add to the pool.
+     * @custom:modifier nonReentrant Function cannot be re-entered
+     */
+    function addLiquidity(uint256 amount0) external nonReentrant {
+        uint256 amount0Scaled = amount0 * PRECISION;
+
+        uint256 amount1Scaled = getAmountOut(amount0Scaled);
+        uint256 amount1 = amount1Scaled / PRECISION;
+
+        uint256 newLiquidity = sqrt(amount0Scaled * amount1Scaled);
+
+        reserve0 += amount0;
+        reserve1 += amount1;
+
+        userEntryFeePerLiquidityUnitToken0[
+            msg.sender
+        ] = accumulatedFeePerLiquidityUnitToken0;
+        userEntryFeePerLiquidityUnitToken1[
+            msg.sender
+        ] = accumulatedFeePerLiquidityUnitToken1;
+        liquidity += newLiquidity;
+        lpBalances[msg.sender] += newLiquidity;
+
+        emit AddLiquidity(msg.sender, amount0, amount1);
+
+        token0.safeTransferFrom(msg.sender, address(this), amount0);
+        token1.safeTransferFrom(msg.sender, address(this), amount1);
+    }
+
+    /**
+     * @notice Removes all liquidity the caller has in the pool.
+     * @dev The functions gets the correct amount of token0 and token1 to send to the user.
+     *      Handles transfers from the pool to the caller.
+     *      Updates reserves, liquidity of the pool.
+     *      Updates lp balance of the caller.
+     *      Emits a `RemoveLiquidity` event upon successful execution.
+     * @custom:modifier nonReentrant Function cannot be re-entered.
+     * @custom:revert InsufficentLiquidity if the caller has no liquidity in the pool.
+     */
+    function removeLiquidity() external nonReentrant {
+        uint256 liquidityToRemove = lpBalances[msg.sender];
+        if (liquidityToRemove <= 0) {
+            revert InsufficentLiquidity();
+        }
+        uint256 amount0 = (reserve0 * liquidityToRemove) / liquidity;
+        uint256 amount1 = (reserve1 * liquidityToRemove) / liquidity;
+
+        reserve0 -= amount0;
+        reserve1 -= amount1;
+
+        liquidity -= liquidityToRemove;
+        lpBalances[msg.sender] -= liquidityToRemove;
+
+        emit RemoveLiquidity(msg.sender, liquidityToRemove, amount0, amount1);
+
+        token0.safeTransfer(msg.sender, amount0);
+        token1.safeTransfer(msg.sender, amount1);
+    }
+
+    /**
+     * @notice Swaps amount of token0 for an amount of token1
+     * @dev The function calculates the proper amount of token1 and swaps it with
+     *      the caller for his provided amount of token0.
+     *      It calls getAmountOut to get the correct amount of token1 in proportion to token0.
+     *      Rounds this result down and calculates the correct amout of token0 to be traded for the amount.
+     *      This way the swap is fair and there is minimal slippage.
+     *      Calculates the fee amount and updates the fee pool.
+     *      Transfers amount token0 to the pool and transfers amount token1 to the caller.
+     *      Updates reserves of the pool.
+     *      Emits a `Swap` event upon successful execution.
+     * @param amountIn The amount of token0 to add to the pool.
+     * @custom:modifier nonReentrant Function cannot be re-entered
+     * @custom:revert "Invalid output amount" if the calculated amount of token1 is less than 0
+     */
+    function swapToken0Amount(uint256 amountIn) external nonReentrant {
+        uint256 scaledAmountIn = amountIn * PRECISION;
+
+        uint256 amountOutScaled = getAmountOut(scaledAmountIn);
+        if (amountOutScaled < PRECISION) revert("Amount out too small");
+        uint256 amountOutRounded = roundDownToNearestWhole(amountOutScaled);
+        uint256 amountOut = amountOutRounded / PRECISION;
+
+        uint256 amountInCorrect = getAmountIn(amountOutRounded);
+
+        uint256 feeAmount = (amountInCorrect * FEE) / ONE_HUNDRED;
+        uint256 amountInAfterFee = amountInCorrect + feeAmount;
+
+        uint256 amountInRouded = roundUpToNearestWhole(amountInAfterFee);
+        uint256 amountInSlippageFree = amountInRouded / PRECISION;
+
+        if (amountOut <= 0) revert("Invalid output amount");
+
+        accumulatedFeePerLiquidityUnitToken0 +=
+            (feeAmount * PRECISION) /
+            liquidity;
+
+        reserve0 += scaledAmountIn / PRECISION;
+        reserve1 -= amountOut;
+
+        emit Swap(msg.sender, amountInSlippageFree, amountOut);
+
+        token0.safeTransferFrom(
+            msg.sender,
+            address(this),
+            amountInSlippageFree
+        );
+        token1.safeTransfer(msg.sender, amountOut);
+    }
+
+    /**
+     * @notice Swaps amount of token1 for an amount of token0
+     * @dev The function calculates the proper amount of token0 and swaps it with
+     *      the caller for his provided amount of token1.
+     *      It calls getAmountIn to get the correct amount of token0 in proportion to token1.
+     *      Rounds this result down and calculates the correct amout of token1 to be traded for the amount.
+     *      This way the swap is fair and there is minimal slippage.
+     *      Calculates the fee amount and updates the fee pool.
+     *      Transfers amount token1 to the pool and transfers amount token0 to the caller.
+     *      Updates reserves of the pool.
+     *      Emits a `Swap` event upon successful execution.
+     * @param amountIn The amount of token0 to add to the pool.
+     * @custom:modifier nonReentrant Function cannot be re-entered
+     * @custom:revert "Invalid output amount" if the calculated amount of token1 is less than 0
+     */
+    function swapToken1Amount(
+        uint256 amountIn,
+        uint256 amountInMax
+    ) external nonReentrant {
+        uint256 scaledAmountIn = amountIn * PRECISION;
+
+        uint256 amountOutScaled = getAmountIn(scaledAmountIn);
+        uint256 amountOutRounded = roundDownToNearestWhole(amountOutScaled);
+        uint256 amountOut = amountOutRounded / PRECISION;
+
+        uint256 amountInCorrect = getAmountOut(amountOutRounded);
+
+        uint256 feeAmount = (amountInCorrect * FEE) / ONE_HUNDRED;
+        uint256 amountInAfterFee = amountInCorrect + feeAmount;
+
+        uint256 amountInRouded = roundUpToNearestWhole(amountInAfterFee);
+        uint256 amountInSlippageFree = amountInRouded / PRECISION;
+
+        if (amountOut <= 0) revert("Invalid output amount");
+
+        accumulatedFeePerLiquidityUnitToken1 +=
+            (feeAmount * PRECISION) /
+            liquidity;
+
+        reserve1 += scaledAmountIn / PRECISION;
+        reserve0 -= amountOut;
+
+        emit Swap(msg.sender, amountInSlippageFree, amountOut);
+
+        token1.safeTransferFrom(
+            msg.sender,
+            address(this),
+            amountInSlippageFree
+        );
+        token0.safeTransfer(msg.sender, amountOut);
+    }
+
+    /**
+     * @notice Withdraws collected fees of the caller
+     * @dev The function calculates the callers share of the fee pool and
+     *      transfers the fees from the pool to the caller.
+     *      Updates the liquidity entry point for the the caller so he can't double spend.
+     *      Emits a `WithdrawFees` event upon successful execution.
+     * @custom:modifier nonReentrant Function cannot be re-entered
+     * @custom:revert InsufficentFeesBalance if the calculated fee share is less than 0
+     */
+    function withdrawFees() external nonReentrant {
+        uint256 liquidityToRemove = lpBalances[msg.sender];
+
+        uint256 feeShareScaledToken0 = ((accumulatedFeePerLiquidityUnitToken0 -
+            userEntryFeePerLiquidityUnitToken0[msg.sender]) *
+            liquidityToRemove) / PRECISION;
+        uint256 feeShareToken0 = feeShareScaledToken0 / PRECISION;
+
+        uint256 feeShareScaledToken1 = ((accumulatedFeePerLiquidityUnitToken1 -
+            userEntryFeePerLiquidityUnitToken1[msg.sender]) *
+            liquidityToRemove) / PRECISION;
+        uint256 feeShareToken1 = feeShareScaledToken1 / PRECISION;
+
+        if (feeShareToken0 <= 0 && feeShareToken1 <= 0) {
+            revert InsufficentFeesBalance();
+        }
+
+        if (feeShareToken0 > 0) {
+            userEntryFeePerLiquidityUnitToken0[
+                msg.sender
+            ] = accumulatedFeePerLiquidityUnitToken0;
+            emit WithdrawFees(msg.sender, feeShareToken0);
+            token0.safeTransfer(msg.sender, feeShareToken0);
+        }
+
+        if (feeShareToken1 > 0) {
+            userEntryFeePerLiquidityUnitToken1[
+                msg.sender
+            ] = accumulatedFeePerLiquidityUnitToken1;
+
+            emit WithdrawFees(msg.sender, feeShareToken0);
+
+            token1.safeTransfer(msg.sender, feeShareToken1);
+        }
+    }
+
+    /**
+     * @notice Gets amount of token1 compared to amount token0
+     * @param amountIn The amount of token0
+     * @dev The function calculates and returns the token1 compared to token0 amount
+     */
+    function getAmountOut(uint256 amountIn) internal view returns (uint256) {
+        uint256 k = reserve0 * PRECISION * reserve1 * PRECISION;
+        uint256 newReserve0 = reserve0 * PRECISION + amountIn;
+        uint256 newReserve1 = k / newReserve0;
+
+        return reserve1 * PRECISION - newReserve1;
+    }
+
+    /**
+     * @notice Gets amount of token0 compared to amount token1
+     * @dev The function calculates and returns the token0 compared to token1 amount
+     * @param amountOut The amount of token1
+     */
+    function getAmountIn(uint256 amountOut) internal view returns (uint256) {
+        uint256 k = reserve0 * PRECISION * reserve1 * PRECISION;
+        uint256 newReserve1 = reserve1 * PRECISION - amountOut;
+        uint256 newReserve0 = k / newReserve1;
+
+        return newReserve0 - reserve0 * PRECISION;
+    }
+
+    /**
+     * @notice Helper function for rounding up numbers
+     * @dev The function rounds up the provided number and returns it
+     * @param value The amount of token0
+     */
+    function roundUpToNearestWhole(
+        uint256 value
+    ) internal pure returns (uint256) {
+        // If there's any remainder when dividing by PRECISION, round up
+        if (value % PRECISION != 0) {
+            return ((value / PRECISION) + 1) * PRECISION;
+        }
+        return value;
+    }
+
+    /**
+     * @notice Helper function for rounding up numbers
+     * @dev The function rounds down the provided number and returns it
+     * @param value The amount of token0
+     */
+    function roundDownToNearestWhole(
+        uint256 value
+    ) internal pure returns (uint256) {
+        // Divide and multiply to get the rounded down value
+        return (value / PRECISION) * PRECISION;
+    }
+
+    /**
+     * @notice Helper function for calculating the square root of a number
+     * @dev The function rounds up the provided number and returns it
+     * @param x The number to calculate the square root for
+     */
+    function sqrt(uint256 x) internal pure returns (uint256 y) {
+        y = x;
+        uint256 z = (x + 1) / 2;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    }
+}
---
+++ test/JoesSwapV3Test.sol
@@ -0,0 +1,231 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {JoesSwapV3} from "../src/JoesSwapV3.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20Mock} from "./ERC20Mock.sol";
+
+contract JoesSwapV3Test is Test {
+    JoesSwapV3 joesSwapV3;
+    ERC20Mock token0;
+    ERC20Mock token1;
+
+    uint256 immutable PRECISION = 1e18;
+
+    address owner = address(0x4eFF9F6DBb11A3D9a18E92E35BD4D54ac4E1533a);
+    address owner2 = address(2);
+
+    function setUp() public {
+        token0 = new ERC20Mock("Token0", "T0");
+        token1 = new ERC20Mock("Token1", "T1");
+
+        uint256 STARTING_AMOUNT = 1_000_000;
+
+        vm.deal(owner, STARTING_AMOUNT);
+        vm.deal(owner2, STARTING_AMOUNT);
+
+        token0.mint(owner, STARTING_AMOUNT);
+        token1.mint(owner, STARTING_AMOUNT);
+
+        token0.mint(owner2, STARTING_AMOUNT);
+        token1.mint(owner2, STARTING_AMOUNT);
+
+        vm.prank(owner);
+        joesSwapV3 = new JoesSwapV3(address(token0), address(token1));
+
+        vm.startPrank(owner);
+        token0.approve(address(joesSwapV3), STARTING_AMOUNT);
+        token1.approve(address(joesSwapV3), STARTING_AMOUNT);
+        vm.stopPrank();
+
+        vm.startPrank(owner2);
+        token0.approve(address(joesSwapV3), STARTING_AMOUNT);
+        token1.approve(address(joesSwapV3), STARTING_AMOUNT);
+        vm.stopPrank();
+
+        uint256 amount0 = 10000;
+        uint256 amount1 = 1000;
+        vm.prank(owner);
+        joesSwapV3.initializePoolLiquidity(amount0, amount1);
+        console.log("initialized", joesSwapV3.poolInitialized());
+    }
+
+    function test_initializePoolLiquidityAsNotOwner() public {
+        token0 = new ERC20Mock("Token0", "T0");
+        token1 = new ERC20Mock("Token1", "T1");
+
+        uint256 STARTING_AMOUNT = 1_000_000;
+
+        vm.deal(owner, STARTING_AMOUNT);
+        vm.deal(owner2, STARTING_AMOUNT);
+
+        token0.mint(owner, STARTING_AMOUNT);
+        token1.mint(owner, STARTING_AMOUNT);
+
+        token0.mint(owner2, STARTING_AMOUNT);
+        token1.mint(owner2, STARTING_AMOUNT);
+
+        vm.prank(owner);
+        joesSwapV3 = new JoesSwapV3(address(token0), address(token1));
+
+
+        vm.startPrank(owner);
+        token0.approve(address(joesSwapV3), STARTING_AMOUNT);
+        token1.approve(address(joesSwapV3), STARTING_AMOUNT);
+        vm.stopPrank();
+
+        vm.startPrank(owner2);
+        token0.approve(address(joesSwapV3), STARTING_AMOUNT);
+        token1.approve(address(joesSwapV3), STARTING_AMOUNT);
+        vm.stopPrank();
+
+        uint256 amount0 = 10000;
+        uint256 amount1 = 1000;
+
+        vm.prank(owner2);
+        vm.expectRevert();
+        joesSwapV3.initializePoolLiquidity(amount0, amount1);
+    }
+
+    function test_initializePoolTwice() public {
+        uint256 amount0 = 10000;
+        uint256 amount1 = 1000;
+
+        console.log("initialized", joesSwapV3.poolInitialized());
+        console.log("liquidity", joesSwapV3.liquidity());
+        vm.prank(owner);
+       vm.expectRevert();
+        joesSwapV3.initializePoolLiquidity(amount0, amount1);
+    }
+
+    function test_addLiquidity() public {
+        uint256 amount0 = 500;
+        uint256 reserve0Before = joesSwapV3.reserve0();
+
+        vm.prank(owner);
+        joesSwapV3.addLiquidity(amount0);
+
+        assertEq(joesSwapV3.reserve0(), reserve0Before + amount0);
+    }
+
+    function test_removeLiquidity() public {
+        uint256 liquidityBefore = joesSwapV3.liquidity();
+
+        vm.prank(owner);
+        joesSwapV3.removeLiquidity();
+    }
+
+    function test_swapToken0Amount() public {
+        uint256 swapAmount = 1000;
+        uint256 liquidityBefore = joesSwapV3.liquidity();
+        uint256 reserve0Before = joesSwapV3.reserve0();
+        uint256 reserve1Before = joesSwapV3.reserve1();
+
+        vm.prank(owner);
+        joesSwapV3.swapToken0Amount(swapAmount);
+
+        uint256 reserve1AfterExpected = (reserve0Before * reserve1Before) /
+            (reserve0Before + swapAmount);
+
+        //        assertEq(joesSwapV3.liquidity(), liquidityBefore);
+        //        assertEq(joesSwapV3.reserve0(), reserve0Before + swapAmount);
+        //        assertEq(joesSwapV3.reserve1(), reserve1AfterExpected);
+
+        vm.prank(owner);
+        joesSwapV3.removeLiquidity();
+    }
+
+    function test_swapToken1Amount_1() public {
+        uint256 swapAmount = 10;
+        uint256 liquidityBefore = joesSwapV3.liquidity();
+        uint256 reserve0Before = joesSwapV3.reserve0();
+        uint256 reserve1Before = joesSwapV3.reserve1();
+
+        vm.prank(owner);
+        joesSwapV3.swapToken1Amount(swapAmount, 123);
+
+        uint256 reserve1AfterExpected = (reserve0Before * reserve1Before) /
+            (reserve0Before + swapAmount);
+
+        //        assertEq(joesSwapV3.liquidity(), liquidityBefore);
+        //        assertEq(joesSwapV3.reserve0(), reserve0Before + swapAmount);
+        //        assertEq(joesSwapV3.reserve1(), reserve1AfterExpected);
+
+        vm.prank(owner);
+        joesSwapV3.removeLiquidity();
+    }
+
+    function test_swapToken1Amount_2() public {
+        uint256 swapAmount = 333;
+        uint256 liquidityBefore = joesSwapV3.liquidity();
+        uint256 reserve0Before = joesSwapV3.reserve0();
+        uint256 reserve1Before = joesSwapV3.reserve1();
+
+        vm.prank(owner);
+        joesSwapV3.swapToken1Amount(swapAmount, 10000);
+
+        uint256 reserve1AfterExpected = (reserve0Before * reserve1Before) /
+            (reserve0Before + swapAmount);
+
+        //        assertEq(joesSwapV3.liquidity(), liquidityBefore);
+        //        assertEq(joesSwapV3.reserve0(), reserve0Before + swapAmount);
+        //        assertEq(joesSwapV3.reserve1(), reserve1AfterExpected);
+
+        uint256 liquidity = joesSwapV3.liquidity();
+        vm.prank(owner);
+        joesSwapV3.withdrawFees();
+        //        vm.prank(owner);
+        //        joesSwapV3.removeLiquidity();
+    }
+
+    function test_swapToken1Amount_3() public {
+        uint256 swapAmount = 100;
+        uint256 amount0 = 10000;
+
+        vm.prank(owner);
+        joesSwapV3.swapToken1Amount(swapAmount, 10000);
+
+        vm.prank(owner2);
+        joesSwapV3.addLiquidity(amount0);
+
+        vm.prank(owner);
+        joesSwapV3.swapToken1Amount(swapAmount, 10000);
+
+        vm.prank(owner);
+        joesSwapV3.swapToken1Amount(swapAmount, 10000);
+
+        vm.prank(owner);
+        joesSwapV3.withdrawFees();
+        vm.prank(owner2);
+        joesSwapV3.withdrawFees();
+        vm.prank(owner);
+        joesSwapV3.removeLiquidity();
+
+        vm.prank(owner);
+        joesSwapV3.swapToken1Amount(swapAmount, 10000);
+
+        vm.prank(owner2);
+        joesSwapV3.withdrawFees();
+
+        vm.prank(owner);
+        vm.expectRevert();
+        joesSwapV3.withdrawFees();
+    }
+
+    function test_withdrawFeesBeforeSwap() public {
+        vm.prank(owner);
+        vm.expectRevert();
+        joesSwapV3.withdrawFees();
+    }
+
+    function sqrt(uint256 x) internal pure returns (uint256 y) {
+        y = x;
+        uint256 z = (x + 1) / 2;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    }
+}
+
