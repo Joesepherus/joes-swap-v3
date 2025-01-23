50904646776c68c775069121622acebc28f1e65d
joesepherus <joesepherus@gmail.com>
2025-01-23 19:22:36 +0800

    Add JoesSwapFactory.sol

---
+++ src/JoesSwapFactory.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+import {JoesSwapV3} from "./JoesSwapV3.sol";
+
+contract JoesSwapFactory {
+    mapping(address=>mapping(address=>address)) public getPool;
+    address[] public pools;
+    event CreatedPool(address tokenA, address tokenB, address pool);
+
+    function createPool(address tokenA, address tokenB) external returns(address pool) {
+        require(tokenA != address(0), "Invalid address fot tokenA.");
+        require(tokenB != address(0), "Invalid address for tokenB.");
+        require(tokenA!=tokenB, "Identical tokens.");
+        require(getPool[tokenA][tokenB]==address(0), "Pair exists.");
+
+        pool = address(new JoesSwapV3(tokenA, tokenB));
+        getPool[tokenA][tokenB] = address(pool);
+        pools.push(pool);
+        emit CreatedPool(tokenA, tokenB, pool);
+    }
+}
