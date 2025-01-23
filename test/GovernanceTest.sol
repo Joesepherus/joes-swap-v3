1f895b9490bcfde1422a52592798bde6a026222a
joesepherus <joesepherus@gmail.com>
2025-01-23 21:30:32 +0800

    Update GovernanceTests to have the new data and proposalType fields.

--- test/GovernanceTest.sol
+++ test/GovernanceTest.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {Governance} from "../src/Governance.sol";
 import {ERC20Mock} from "./ERC20Mock.sol";
+import {JoesSwapFactory} from "../src/JoesSwapFactory.sol";
 
-contract JoesSwapV3Test is Test {
+contract GovernanceTest is Test {
     Governance governance;
     ERC20Mock token;
 
-    address USER = address(0x4eFF9F6DBb11A3D9a18E92E35BD4D54ac4E1533a);
+    address OWNER = address(0x4eFF9F6DBb11A3D9a18E92E35BD4D54ac4E1533a);
+    address USER = address(1);
     address USER2 = address(2);
 
     uint256 STARTING_AMOUNT = 1_000;
@@ -22,13 +24,25 @@ contract JoesSwapV3Test is Test {
     }
 
     function test_createProposal() public {
-        governance.createProposal("Increase fee to 5%.");
+        uint256 fee = 5;
+        bytes memory data = abi.encode(fee);
+        governance.createProposal(
+            "Increase fee to 5%.",
+            Governance.ProposalType.CHANGE_FEE,
+            data
+        );
         Governance.Proposal memory proposal = governance.getProposal(1);
         console.log("proposal", proposal.description);
     }
 
     function test_voteOnce() public {
-        governance.createProposal("Increase fee to 5%.");
+        uint256 fee = 5;
+        bytes memory data = abi.encode(fee);
+        governance.createProposal(
+            "Increase fee to 5%.",
+            Governance.ProposalType.CHANGE_FEE,
+            data
+        );
         Governance.Proposal memory proposal = governance.getProposal(1);
         console.log("proposal", proposal.description);
         vm.prank(USER);
@@ -36,7 +50,13 @@ contract JoesSwapV3Test is Test {
     }
 
     function test_voteTwice() public {
-        governance.createProposal("Increase fee to 5%.");
+        uint256 fee = 5;
+        bytes memory data = abi.encode(fee);
+        governance.createProposal(
+            "Increase fee to 5%.",
+            Governance.ProposalType.CHANGE_FEE,
+            data
+        );
         Governance.Proposal memory proposal = governance.getProposal(1);
         console.log("proposal", proposal.description);
         vm.startPrank(USER);
@@ -46,7 +66,13 @@ contract JoesSwapV3Test is Test {
     }
 
     function test_voteAfterDeadline() public {
-        governance.createProposal("Increase fee to 5%.");
+        uint256 fee = 5;
+        bytes memory data = abi.encode(fee);
+        governance.createProposal(
+            "Increase fee to 5%.",
+            Governance.ProposalType.CHANGE_FEE,
+            data
+        );
         Governance.Proposal memory proposal = governance.getProposal(1);
         console.log("proposal", proposal.description);
         vm.warp(8 days);
@@ -56,7 +82,13 @@ contract JoesSwapV3Test is Test {
     }
 
     function test_executeProposal() public {
-        governance.createProposal("Increase fee to 5%.");
+        uint256 fee = 5;
+        bytes memory data = abi.encode(fee);
+        governance.createProposal(
+            "Increase fee to 5%.",
+            Governance.ProposalType.CHANGE_FEE,
+            data
+        );
         Governance.Proposal memory proposal = governance.getProposal(1);
         console.log("proposal", proposal.description);
         vm.startPrank(USER);
@@ -65,8 +97,14 @@ contract JoesSwapV3Test is Test {
     }
 
     function test_executeProposalFail() public {
+        uint256 fee = 5;
+        bytes memory data = abi.encode(fee);
+        governance.createProposal(
+            "Increase fee to 5%.",
+            Governance.ProposalType.CHANGE_FEE,
+            data
+        );
         token.mint(USER2, STARTING_AMOUNT);
-        governance.createProposal("Increase fee to 5%.");
         Governance.Proposal memory proposal = governance.getProposal(1);
         console.log("proposal", proposal.description);
         vm.startPrank(USER);
@@ -75,5 +113,25 @@ contract JoesSwapV3Test is Test {
         governance.executeProposal(1);
     }
 
-
+    function test_executeProposalCreatePool() public {
+        ERC20Mock ETH = new ERC20Mock("Ethereum", "ETH");
+        ERC20Mock USD = new ERC20Mock("US dolla", "USD");
+        bytes memory data = abi.encode(ETH, USD);
+        governance.createProposal(
+            "Create new pool for ETH:USD.",
+            Governance.ProposalType.CREATE_POOL,
+            data
+        );
+        Governance.Proposal memory proposal = governance.getProposal(1);
+        console.log("proposal", proposal.description);
+        vm.startPrank(USER);
+        governance.vote(1);
+        governance.executeProposal(1);
+        JoesSwapFactory joesSwapFactory = governance.joesSwapFactory();
+        address ETHUSDPool = joesSwapFactory.getPool(
+            address(ETH),
+            address(USD)
+        );
+        assert(ETHUSDPool != address(0));
+    }
 }
