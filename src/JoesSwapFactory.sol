9a90d5599b03a78a20dff5baabf5d19de3bd76ef
joesepherus <joesepherus@gmail.com>
2025-01-23 19:37:02 +0800

    Add isOwnable to JoesSwapFactory.

--- src/JoesSwapFactory.sol
+++ src/JoesSwapFactory.sol
@@ -1,17 +1,23 @@
 pragma solidity ^0.8.0;
 
 import {JoesSwapV3} from "./JoesSwapV3.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract JoesSwapFactory {
-    mapping(address=>mapping(address=>address)) public getPool;
+contract JoesSwapFactory is Ownable {
+    mapping(address => mapping(address => address)) public getPool;
     address[] public pools;
     event CreatedPool(address tokenA, address tokenB, address pool);
 
-    function createPool(address tokenA, address tokenB) external returns(address pool) {
+    constructor() Ownable(msg.sender) {}
+
+    function createPool(
+        address tokenA,
+        address tokenB
+    ) external returns (address pool) {
         require(tokenA != address(0), "Invalid address fot tokenA.");
         require(tokenB != address(0), "Invalid address for tokenB.");
-        require(tokenA!=tokenB, "Identical tokens.");
-        require(getPool[tokenA][tokenB]==address(0), "Pair exists.");
+        require(tokenA != tokenB, "Identical tokens.");
+        require(getPool[tokenA][tokenB] == address(0), "Pair exists.");
 
         pool = address(new JoesSwapV3(tokenA, tokenB));
         getPool[tokenA][tokenB] = address(pool);
