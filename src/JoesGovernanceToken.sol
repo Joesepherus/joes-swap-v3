f68737823c212fa4f5c58c40d849d564879a1c39
joesepherus <joesepherus@gmail.com>
2025-02-03 02:36:29 +0800

    Add JoesGovernanceToken.

---
+++ src/JoesGovernanceToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract JoesGovernanceToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Joes Governance Token", "JGT") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+}
