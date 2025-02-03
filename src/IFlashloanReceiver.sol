90a38f3f2057bac25ad3acf4128f1547147c822d
joesepherus <joesepherus@gmail.com>
2025-02-03 15:28:19 +0800

    Add pragma to IFlashloanReceiver.

--- src/IFlashloanReceiver.sol
+++ src/IFlashloanReceiver.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
 // Interface of the other contract
 interface IFlashloanReceiver {
     function flashloan_receive(uint256 amount, address token) external;
