dcd2b3cf247586129bca7f79bea1cec45a286e38
joesepherus <joesepherus@gmail.com>
2025-02-05 21:21:17 +0800

    Add natspec for IFlashloanReceiver.

--- src/IFlashloanReceiver.sol
+++ src/IFlashloanReceiver.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
 
-// Interface of the other contract
+/**
+ * @title IFlashloanReceiver
+ * @author Joesepherus
+ * @dev Interface for contracts that receive flash loans.
+ */
 interface IFlashloanReceiver {
+    /**
+     * @notice Called by the flash loan provider when the loan is issued.
+     * @dev The receiver contract must implement this function to handle the flash loan.
+     * @param amount The amount of tokens borrowed in the flash loan.
+     * @param token The address of the token being borrowed.
+     */
     function flashloan_receive(uint256 amount, address token) external;
 }
