aea674dc3d6e1c633bc06021f6abfb31bb6e1a87
joesepherus <joesepherus@gmail.com>
2025-02-05 21:21:11 +0800

    Add natspec for FlashloanReceiver.

--- src/FlashloanReceiver.sol
+++ src/FlashloanReceiver.sol
@@ -7,20 +7,46 @@ import {IFlashloanReceiver} from "./IFlashloanReceiver.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
+/**
+ * @title FlashloanReceiver
+ * @author Joesepherus
+ * @dev Implements the IFlashloanReceiver interface to receive and repay flash loans.
+ */
 contract FlashloanReceiver is IFlashloanReceiver{
     using SafeERC20 for IERC20;
+
+    // @notice The instance of JoesSwapV3 used for flash loans.
     JoesSwapV3 joesSwapV3;
+
+    // @notice The fixed flash loan fee (3%).
     uint256 immutable FEE = 3;
 
+    /**
+     * @dev Sets the JoesSwapV3 contract address.
+     * @param _joesSwapV3 The address of the JoesSwapV3 contract.
+     */
     constructor(address _joesSwapV3) {
         require(_joesSwapV3 != address(0), "Not a valid address.");
         joesSwapV3 = JoesSwapV3(_joesSwapV3);
     }
 
+    
+    /**
+     * @notice Initiates a flash loan from JoesSwapV3.
+     * @dev Calls the flashloan function of JoesSwapV3.
+     * @param amount The amount of tokens to borrow.
+     * @param token The address of the token to borrow.
+     */
     function flashloan(uint256 amount, address token) external {
         joesSwapV3.flashloan(amount, token);
     }
 
+    /**
+     * @notice Handles the flash loan callback, repaying the borrowed amount plus fees.
+     * @dev Transfers the borrowed amount plus a 3% fee back to the JoesSwapV3 contract.
+     * @param amount The amount borrowed.
+     * @param token The address of the borrowed token.
+     */
     function flashloan_receive(uint256 amount, address token) external {
         uint256 fee = (amount * FEE) / 100;
 
